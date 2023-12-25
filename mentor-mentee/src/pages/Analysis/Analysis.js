56a6881b7ee5ec41bd7309d66fe4e5b46c2996f3
Prajwal Waghmode <prajwalwaghmode10@gmail.com>
2023-12-25 23:11:10 +0530

    updates to Analysis.js

--- mentor-mentee/src/pages/Analysis/Analysis.js
+++ mentor-mentee/src/pages/Analysis/Analysis.js
@@ -1,4 +1,10 @@
-import React, { useEffect, useRef, useMemo, useCallback } from "react";
+import React, {
+  useEffect,
+  useRef,
+  useMemo,
+  useCallback,
+  useState,
+} from "react";
 import EngagementActivities from "./EngagementActivities";
 import Marks from "./marks";
 import Attendance from "./Attendance";
@@ -10,8 +16,34 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
 
+const dummyStudentData = [
+  {
+    id: 1,
+    name: "Prajwal Waghmode",
+    prnNumber: "12345",
+  },
+  {
+    id: 2,
+    name: "Arya Angane",
+    prnNumber: "67890",
+  },
+  {
+    id: 3,
+    name: "Astha Thakur",
+    prnNumber: "54321",
+  },
+  {
+    id: 4,
+    name: "Akshija Shetty",
+    prnNumber: "98765",
+  },
+];
+
 function Analysis() {
   const { setActiveSection } = useActiveSection();
+
+  const [searchInput, setSearchInput] = useState("");
+  const [studentData, setStudentData] = useState(null);
 
   const engagementRef = useRef(null);
   const attendanceRef = useRef(null);
@@ -63,70 +95,93 @@ function Analysis() {
     };
   }, [sectionRefs, observer, setActiveSection]);
 
+  const handleSearch = () => {
+    const matchingStudent = dummyStudentData.find(
+      (student) =>
+        student.name.toLowerCase().includes(searchInput.toLowerCase()) ||
+        student.prnNumber.includes(searchInput)
+    );
+
+    setStudentData(matchingStudent || null);
+    setSearchInput("");
+  };
+
   return (
     <div className="bg-secondary">
+      {!studentData && (
+        <h3 className="text-center text-dark pt-4">
+          Search Student Data for their Data Analysis
+        </h3>
+      )}
       <Form>
         <div className="d-flex justify-content-center mb-5">
-          <Row className="align-items-center mt-3">
+          <Row className="align-items-center mt-4 mb-2">
             <Col xs="auto">
               <Form.Control
                 type="text"
                 placeholder="Search Student"
                 style={{ width: "300px" }}
                 className="sm-3"
+                value={searchInput}
+                onChange={(e) => setSearchInput(e.target.value)}
               />
             </Col>
             <Col className="xs-5">
-              <Button variant="warning">Search</Button>
+              <Button variant="warning" onClick={handleSearch}>
+                Search
+              </Button>
             </Col>
           </Row>
         </div>
-        <div className="ms-4">
-          <Form.Group className="d-flex mb-3">
-            <Form.Label className="col-md-2 text-center p-1 me-4">
-              Name :
-            </Form.Label>
-            <div className="col-md-5">
-              <Form.Control
-                type="text"
-                placeholder="Display Name here"
-                readOnly
-              />
-            </div>
-          </Form.Group>
-          <Form.Group className="d-flex mb-3">
-            <Form.Label className="col-md-2 text-center p-1 me-4">
-              PRN Number :
-            </Form.Label>
-            <div className="col-md-5">
-              <Form.Control
-                type="text"
-                placeholder="Display Roll Number here"
-                readOnly
-              />
-            </div>
-          </Form.Group>
-        </div>
+        {studentData && (
+          <div className="ms-4 me-3">
+            <Form.Group className="d-flex flex-column flex-md-row mb-3">
+              <Form.Label className="col-md-2 text-md-center p-1 me-md-4 mb-2 mb-md-0">
+                Name :
+              </Form.Label>
+              <div className="col-md-5">
+                <Form.Control type="text" value={studentData.name} readOnly />
+              </div>
+            </Form.Group>
+            <Form.Group className="d-flex flex-column flex-md-row mb-3">
+              <Form.Label className="col-md-2 text-md-center p-1 me-md-4 mb-2 mb-md-0">
+                PRN Number :
+              </Form.Label>
+              <div className="col-md-5 ">
+                <Form.Control
+                  type="text"
+                  value={studentData.prnNumber}
+                  readOnly
+                />
+              </div>
+            </Form.Group>
+          </div>
+        )}
       </Form>
-      <div ref={engagementRef} id="engagement-activities">
-        <EngagementActivities />
-      </div>
-      <hr />
-      <div ref={attendanceRef} id="attendance">
-        <Attendance />
-      </div>
-      <hr />
-      <div ref={marksRef} id="marks">
-        <Marks />
-      </div>
-      <hr />
-      <div ref={coursesRef} id="courses">
-        <Courses />
-      </div>
-      <hr />
-      <div ref={publicationsRef} id="publications">
-        <Publications />
-      </div>
+      {studentData && (
+        <>
+          <hr />
+          <div ref={engagementRef} id="engagement-activities">
+            <EngagementActivities />
+          </div>
+          <hr />
+          <div ref={attendanceRef} id="attendance">
+            <Attendance />
+          </div>
+          <hr />
+          <div ref={marksRef} id="marks">
+            <Marks />
+          </div>
+          <hr />
+          <div ref={coursesRef} id="courses">
+            <Courses />
+          </div>
+          <hr />
+          <div ref={publicationsRef} id="publications">
+            <Publications />
+          </div>
+        </>
+      )}
     </div>
   );
 }
